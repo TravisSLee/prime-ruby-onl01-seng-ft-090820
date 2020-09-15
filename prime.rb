18ae58660e152b4062274651a7ec1e6870322efe
Travis Lee <travisslee223@gmail.com>
2020-09-15 16:17:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,16 @@
 # Add  code here!
 def prime?(number)
+<<<<<<< HEAD
   if number > 1 
      (2..number - 1).none? do |ex|
       number % ex == 0
     end
   else
     false 
+=======
+  primes = [2, 3, 5, 7, 11, 13]
+  primes.none? do |prime|
+    number == prime
+>>>>>>> 4747dfdaa103eb33723d3ad18c943b9f2c896462
   end
 end
